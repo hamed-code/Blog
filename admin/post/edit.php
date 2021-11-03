b7de19a0299c0d7373057879d71bde4e1c3e84c8
morning coder <hamedmessi30@gmail.com>
2021-11-03 08:21:04 +0330

    complete register page

--- admin/post/edit.php
+++ admin/post/edit.php
@@ -71,8 +71,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PHP panel</title>
-    <link rel="stylesheet" href="../../assets/css/bootstrap.min.css" media="all" type="text/css">
-    <link rel="stylesheet" href="../../assets/css/style.css" media="all" type="text/css">
+    <link rel="stylesheet" href="<?= asset("assets/css/bootstrap.min.css"); ?>" media="all" type="text/css">
+    <link rel="stylesheet" href="<?= asset("assets/css/style.css"); ?>" media="all" type="text/css">
 </head>
 
 <body>
@@ -132,8 +132,8 @@
 
     </section>
 
-    <script src="../../assets/js/jquery.min.js"></script>
-    <script src="../../assets/js/bootstrap.min.js"></script>
+    <script src="<?= asset('assets/js/jquery.min.js') ?>"></script>
+    <script src="<?= asset('assets/js/bootstrap.min.js') ?>"></script>
 </body>
 
 </html>
